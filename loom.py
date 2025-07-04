a1e36947fd91c191a0aa4cdb8330c7632f5cd6c0
yozba <zitome207@gmail.com>
2025-07-04 19:46:32 +0900

    Fix crash when accessing sequence_editor.sequences on scenes without VSE new scene doesn't have `scn.sequence_editor`, so an error occurs at `len(scn.sequence_editor.sequences)`

--- loom.py
+++ loom.py
@@ -1233,7 +1233,7 @@ class LOOM_OT_render_dialog(bpy.types.Operator):
         
         """ Scene validation """
         if scn.render.use_sequencer and hasattr(scn, "sequencer"):
-            if len(scn.sequence_editor.sequences) and self.validate_sequencer(context):
+            if scn.sequence_editor and len(scn.sequence_editor.sequences) and self.validate_sequencer(context):
                 if not user_input.isdigit():
                     self.report(
                         {'INFO'}, 
@@ -3984,7 +3984,7 @@ class LOOM_OT_render_image_sequence(bpy.types.Operator):
 
         """ Scene validation in case the operator is called via console """
         if self.validate_scene:
-            if scn.render.use_sequencer and len(scn.sequence_editor.sequences):
+            if scn.sequence_editor and scn.render.use_sequencer and len(scn.sequence_editor.sequences):
                 if self.validate_sequencer(context):
                     if len(self._frames) > 1 and not self.render_silent:
                         self.report(
